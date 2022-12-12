c6dff9f80df1423a529c95029ba561f5e209ca67
HappyBell92 <kimi.kaleva.r@gmail.com>
2022-12-12 17:29:46 +0200

    Main Menu Diddledee

---
+++ Assets/Scripts/MainMenuOtherScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuOtherScript : MonoBehaviour
+{
+    public bool mouseOn;
+    public PhysicalPlayButton playButton;
+    public GameObject fx;
+    Animator playGrow;
+    Animator ship;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mouseOn = false;
+        playGrow = GameObject.Find("PlayPhysicalButton").GetComponent<Animator>();
+        ship = GameObject.Find("ShipObjectForMenu").GetComponent<Animator>();
+        fx.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 100, Color.red);
+            if(hit.transform.tag == "PlayButton")
+            {
+                mouseOn = true;
+                playGrow.SetBool("mouseOn", true);
+                if (Input.GetMouseButtonDown(0))
+                {
+                    playButton.PlayGame();
+                }
+            }
+
+            if (hit.transform.tag != "PlayButton")
+            {
+                mouseOn = false;
+                playGrow.SetBool("mouseOn", false);
+            }
+
+                if (hit.transform.tag == "QuitButton")
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    playButton.QuitGame();
+                }
+            }
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
---
+++ Assets/Scripts/PhysicalPlayButton.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PhysicalPlayButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("BOOP!");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game!");
+    }
+}
