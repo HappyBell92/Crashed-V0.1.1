bdcb81dc1dad57c93520068223e2ddcbabceb41d
HappyBell92 <kimi.kaleva.r@gmail.com>
2022-12-16 13:38:10 +0200

    Fixed some stuff needs more fixing when Joel is donezo

--- Assets/Scripts/TeleportScript.cs
+++ Assets/Scripts/TeleportScript.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
-using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class TeleportScript : MonoBehaviour
